66fbb4d1db53812911af065d66e32bbc369715a9
diego-abuv <abuvitordiego.contato@gmail.com>
2024-02-02 08:56:35 -0300

    edição e deleção de usuário adicionadas

--- modules/express.js
+++ modules/express.js
@@ -29,6 +29,28 @@ app.post(`/users`, async (req, res) => {
   }
 });
 
+// Endpoint de edição de usuário
+app.patch(`/users/:id`, async (req, res) => {
+  try {
+    const id = req.params.id
+    const user = await UserModel.findByIdAndUpdate(id, req.body)
+    res.status(200).json(user)
+  } catch (error) {
+    res.status(500).send(error.message)
+  }
+})
+
+// Endpoint de deleção de Usuário
+app.delete(`/users/:id`, async (req, res) => {
+  try {
+    const id = req.params.id
+    const user = await UserModel.findByIdAndDelete(id)
+    res.status(200).json(user)
+  } catch (error) {
+    res.status(500).send(error.message)
+  }
+})
+
 const port = 8080;
 
 app.listen(port, () => console.log(`Rodando com Express na porta ${port}!`));
